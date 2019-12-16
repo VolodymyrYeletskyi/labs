cec315e99096a31419a404ddcbf0a2fd97626099
Volodymyr.Yeletskyi <eleckiyvladimir@gmail.com>
2019-12-16 20:23:43 +0200

    some fixes

--- lab1/src/app/app.component.ts
+++ lab1/src/app/app.component.ts
@@ -3,37 +3,20 @@ import { Component } from '@angular/core';
 @Component({
     selector: 'my-app',
     template: `	
-				<input type = "text" [(ngModel)] = "t"/>
+				<input type = "text" [(ngModel)] = "input_text"/>
 					<button [disabled] = "isActive()" (click) = "Empty()">Enter</button>
 					
-					<p> {{printText()}}</p>`
+					<p> {{result_text}}</p>`
 })
 export class AppComponent { 
-    t= "";
-	r = "";
+    input_text= "";
+	result_text = "";
 	
 	isActive(): boolean {//button is active when we have a text in input line, else button is not active
-		if (this.t != "")
-		{
-			return false;
-		}
-		else
-		{
-			return true;
-		}
+		return this.input_text == "";
 	}
 	Empty(): void {//empty input line when button is pushed
-		this.r += this.t;
-		this.t = "";
+		this.result_text += this.input_text;
+		this.input_text = "";
 	}	
-	printText(): String {//print input on paragraph
-		if (this.t == "")
-		{
-			return this.r;
-		}
-		else
-		{
-			return this.r + this.t;
-		}
-	}
 }
