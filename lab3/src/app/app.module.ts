81105981f91d8aa51d82529d7bf514b721f340e8
Volodymyr.Yeletskyi <eleckiyvladimir@gmail.com>
2019-12-17 21:28:28 +0200

    some fixes

--- lab3/src/app/app.module.ts
+++ lab3/src/app/app.module.ts
@@ -2,9 +2,14 @@ import { NgModule }      from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule }   from '@angular/forms';
 import { AppComponent }   from './app.component';
+import { InactiveUsers }   from './inactiveUsers';
+import { ActiveUsers }   from './activeUsers';
+import { UserService} from './user.service';
+import { CountService} from './count.service';
 @NgModule({
     imports:      [ BrowserModule, FormsModule ],
-    declarations: [ AppComponent, User ],
+    declarations: [ AppComponent, ActiveUsers, InactiveUsers ],
+	providers: [UserService, CountService],
     bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
